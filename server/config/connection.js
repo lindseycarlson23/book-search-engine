e0bdf8bc3b7f17ea872c77d390124245c1248926
lindseycarlson23 <144490541+lindseycarlson23@users.noreply.github.com>
2023-12-06 18:17:41 -0600

    created resolvers, typedefs, mutations, and queries folders

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://root:root@cluster0.d4jt8qo.mongodb.net/googlebooks');
 
 module.exports = mongoose.connection;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,17 +1,45 @@
 const typeDefs = `
- type User {
+  type User {
     _id: ID
     username: String!
     email: String!
     bookCount: Int
     savedBooks: [Book]
- }
-    type Book {
-        bookId: ID!
-        authors: [String]
-        description: String!
-        title: String!
-        image: String
-        link: String
+  }
+
+  type Book {
+    bookId: ID!
+    authors: [String]
+    description: String!
+    title: String!
+    image: String
+    link: String
+  }
+
+  input BookInput {
+    bookId: ID!
+    authors: [String]
+    description: String!
+    title: String!
+    image: String
+    link: String
+  }
+
+    type Auth {
+        token: ID!
+        user: User
     }
-`
+
+    type Query {
+        me: User
+    }
+
+    type Mutation {
+        addUser(username: String!, email: String!, password: String!): Auth
+        login(email: String!, password: String!): Auth
+        addBook(bookData: BookInput!): User
+        removeBook(bookId: ID!): User
+    }
+`;
+
+module.exports = typeDefs;
